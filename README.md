defbb7516f7424431225385658f820f522cf36ca
Chris Dorward <chris.dorward@aligent.com.au>
2017-10-31 16:11:43 +1030

    yeh. noice.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 # Number Animation
 
 React App bootstrapped with [create-react-app](https://github.com/facebookincubator/create-react-app) which shows how to create a number animation.
+
+Odometer is a Javascript and CSS library for smoothly transitioning numbers. It is free and open source and was developed by HubSpot developers Adam Schwartz (@adamfschwartz) and Zack Bloom (@zackbloom).
--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+/* global Odometer */
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+
+  componentDidMount (){
+      this.odometer = new Odometer({
+        el: this.refs.odo,
+        value: '0'
+      });
+      this.odometer.update(1002225);
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,6 +22,8 @@ class App extends Component {
           className="App-logo"
           alt="logo" />
         <h1 className="App-title">Number Animation</h1>
+        <div ref="odo"></div>
+
       </div>
     );
   }
